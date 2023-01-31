0c6402132e85cfe6befbb750867731cdf6058342
bruno <brunoqueiroz612@gmail.com>
2023-01-31 15:23:26 -0300

    create all articles server route

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,11 +3,17 @@ import { connectToDB } from "./db/dbConnection";
 import cors from "cors";
 import Article from "./db/model/article";
 import md from "markdown-it";
+import BlogHomeData from "./db/model/blogHomeData";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 connectToDB();
+
+app.get("/get-all-articles", async (req, res) => {
+  const allArticles = await BlogHomeData.find();
+  res.json(allArticles[0].homeData);
+});
 
 app.get("/test", async (req, res) => {
   const articleData = await Article.find({
@@ -18,7 +24,7 @@ app.get("/test", async (req, res) => {
   res.json(articleHTML);
 });
 
-app.post("/create-article", (req, res) => {
+app.post("/create-article", async (req, res) => {
   const { title, description, markdown } = req.body;
 
   Article.create({
@@ -27,6 +33,20 @@ app.post("/create-article", (req, res) => {
     markdown: [markdown],
     slug: "everything-you-need-to-know-about-typescript",
   });
+  const newArticleData = {
+    title,
+    description,
+    slug: "test-slug",
+  };
+  console.log(req.body);
+  const blogHomeData = await BlogHomeData.findOne();
+  if (blogHomeData === null) {
+    await BlogHomeData.create({
+      homeData: [],
+    });
+  }
+  blogHomeData?.homeData.push(newArticleData);
+  await blogHomeData?.save();
 });
 
 app.listen(3000, () => {
