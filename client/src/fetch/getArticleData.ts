18dc670394b03e4baf77f0228ca81652c3725ad6
bruno <brunoqueiroz612@gmail.com>
2023-02-03 15:19:45 -0300

    fetch a full article data

--- client/src/fetch/getArticleData.ts
+++ client/src/fetch/getArticleData.ts
@@ -1,5 +1,27 @@
-export const getArticleData = async () => {
-  const response = await fetch("http://localhost:3000/test");
-  const data = await response.json();
+export interface ArticleTypes {
+  title: string;
+  description: string;
+  createAt: Date;
+  _id: string;
+  markdown: [string];
+  slug: string;
+  html: string;
+}
+
+export const getArticleData = async (
+  articleSlug: string,
+  populateInputs?: (
+    title: string,
+    description: string,
+    markdown: string
+  ) => void
+) => {
+  const response = await fetch(
+    `http://localhost:3000/get-article/${articleSlug}`
+  );
+  const data: ArticleTypes = await response.json();
+  if (data.markdown && populateInputs) {
+    populateInputs(data.title, data.description, data.markdown[0]);
+  }
   return data;
 };
